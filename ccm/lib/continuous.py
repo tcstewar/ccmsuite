ecd24ac79913ca087e3f67ef74ffa00b9de047a8
Terry Stewart <tcstewar@uwaterloo.ca>
2016-02-16 21:45:23 -0500

    Added continuous range-finding

--- ccm/lib/continuous.py
+++ ccm/lib/continuous.py
@@ -1,7 +1,9 @@
+import math
+
 import grid
 
 class Body(grid.Body):
-    def go_in_direction(self, dir, distance=1):
+    def go_in_direction(self, dir, distance=1, return_obstacle=False):
     
         dir1=int(dir)
         dir2=(dir1+1)%self.world.directions
@@ -23,17 +25,48 @@ class Body(grid.Body):
                 dist = d
         if closest is not self.cell:
             if closest.wall:
-                return False
+                if return_obstacle:
+                    return closest
+                else:
+                    return False
             else:
                 self.cell=closest
             
         self.x=x
         self.y=y
 
-        return True
+        if return_obstacle:
+            return None
+        else:
+            return True
 
     def go_forward(self, distance=1):
         return self.go_in_direction(self.dir, distance=distance)
 
     def go_backward(self, distance=1):
         return self.go_in_direction(self.dir, distance=-distance)
+
+    def detect(self, direction, max_distance=None):
+        start_x = self.x
+        start_y = self.y
+        cell = self.cell
+        distance = 0.0
+        delta = 1.0
+        min_delta = 1.0 / 64
+        obstacle = None
+        if max_distance is None:
+            max_distance = self.world.width + self.world.height
+
+        while distance < max_distance:
+            obstacle = self.go_in_direction(direction, delta, return_obstacle=True)
+            if obstacle is None:
+                distance += delta
+            elif delta > min_delta:
+                delta = delta / 2
+            else:
+                distance = math.sqrt((start_x-self.x)**2 + (start_y-self.y)**2)
+                break
+        self.cell = cell
+        self.x = start_x
+        self.y = start_y
+        return distance, obstacle
