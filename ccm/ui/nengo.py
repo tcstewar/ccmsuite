96e3ce13cbe8faa651b3e1335c4dd8c1ef72bd02
Terry Stewart <tcstewar@uwaterloo.ca>
2016-02-16 21:20:40 -0500

    Added Nengo user interface support

---
+++ ccm/ui/nengo.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+print 'hi'
+import nengo
+
+print 'hello'
+
+class GridNode(nengo.Node):
+    def __init__(self, world):
+        def svg(t):
+            svg._nengo_html_ = self.generate_svg(world)
+        super(GridNode, self).__init__(svg)
+
+    def generate_svg(self, world):
+        cells = []
+        for i in range(world.width):
+            for j in range(world.height):
+                cell = world.get_cell(i, j)
+                color = cell.color
+                if callable(color):
+                    color = color()
+                cells.append('<rect x=%d y=%d width=1 height=1 style="fill:%s"/>' %
+                    (i, j, color))
+
+        agents = []
+        for agent in world.agents:
+            direction = agent.dir * 360.0 / world.directions
+            color = agent.color
+            if callable(color):
+                color = color()
+            agent = ('<polygon points="0.25,0.25 -0.25,0.25 0,-0.5"'
+                     ' style="fill:%s" transform="translate(%f,%f) rotate(%f)"'
+                     % (color, agent.x+0.5, agent.y+0.5, direction))
+            agents.append(agent)
+
+        svg = '''<svg width="100%%" height="100%%" viewbox="0 0 %d %d">
+            %s
+            %s
+            </svg>''' % (world.width, world.height, 
+                         ''.join(cells), ''.join(agents))
+
+        return svg
