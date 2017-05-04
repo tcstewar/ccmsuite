450e3f3f59a8e3e49d9db35bf78f9747d8bdfa18
Terry Stewart <tcstewar@uwaterloo.ca>
2017-05-04 19:10:52 -0400

    Fixed setup to not miss ccm.display

--- setup.py
+++ setup.py
@@ -1,29 +1,12 @@
-#!/usr/bin/env python
-import sys
-import os
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup
-    from setuptools.command.test import test as TestCommand
-except ImportError:
-    try:
-        from ez_setup import use_setuptools
-        use_setuptools()
-        from setuptools import setup
-        from setuptools.command.test import test as TestCommand
-    except Exception as e:
-        print("Forget setuptools, trying distutils...")
-        from distutils.core import setup
-
-root = os.path.dirname(os.path.realpath(__file__))
-description = "Python Cognitive Modelling Suite "
 setup(
     name="ccmsuite",
     version=0.1,
     author="Terry Stewart",
     author_email="tcstewar@uwaterloo.ca",
-    packages=['ccm'],
+    packages=find_packages(),
     scripts=[],
     url="https://github.com/tcstewar/ccmsuite",
-    description=description,
+    description = "Python Cognitive Modelling Suite"
 )
